78ead67fbace094a75df9fc80a42af46ae8e1bab
ganeshkls138 <38781880+ganeshkls138@users.noreply.github.com>
2021-01-28 04:52:45 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-import {AmplifySignOut , withAuthenticator} from '@aws-amplify/ui-react';
 Amplify.configure(aws_exports);
 Amplify.configure(awsconfig);
 
