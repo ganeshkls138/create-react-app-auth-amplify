b64732f4e2cac76f15cda7384c9f7c1997e4801d
ganeshkls138 <38781880+ganeshkls138@users.noreply.github.com>
2021-01-28 05:06:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
-Amplify.configure(awsconfig);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
-            href="https://www.google.com"
+            href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-           The IOT  things registering site
+            Learn React
           </a>
         </header>
       </div>
