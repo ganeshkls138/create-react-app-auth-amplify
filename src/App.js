205c428e55bf2ff39ca5c3f4e525e5069647917c
ganeshkls138 <38781880+ganeshkls138@users.noreply.github.com>
2021-01-28 04:59:36 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
